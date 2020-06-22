85ae53c119e00efaefa6ad28c25e9ee9016a7bea
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-22 13:37:24 -0700

    adding reactstrap and removing materialize

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import {
+
+} from 'reactstrap'
 
 const Navbar = () => {
     return (
-        <nav>
-        <div className="nav-wrapper">
-          <a href="https://acerjak.github.io/ResponsivePortfolio/index.html" className="brand-logo center">Amanda Cerjak</a>
-          <ul className="left hide-on-med-and-down">
-            <li className="active"><a href="/">About</a></li>
-            <li><a href="/portfolio">Portfolio</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
-        </div>
-      </nav>
+
+
+      //   <nav>
+      //   <div className="nav-wrapper">
+      //     <a href="https://acerjak.github.io/ResponsivePortfolio/index.html" className="brand-logo center">Amanda Cerjak</a>
+      //     <ul className="left hide-on-med-and-down">
+      //       <li className="active"><a href="/">About</a></li>
+      //       <li><a href="/portfolio">Portfolio</a></li>
+      //       <li><a href="/contact">Contact</a></li>
+      //     </ul>
+      //   </div>
+      // </nav>
     )
 }
 
