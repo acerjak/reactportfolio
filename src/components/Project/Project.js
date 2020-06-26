f67db4c509fb72efa13404305ba07d35afd40a0a
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-25 23:24:28 -0700

    updates to content

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -40,7 +40,8 @@ const Project = () => {
             </div>
              <div className="card-reveal">
                 <span className="card-title grey-text text-darken-4 right">X</span>
-                <p>Desciption of Project here.
+                <p>This was a homework assignment for the UCI coding bootcamp. We were asked to create a short web development based quiz utilizing JavaScript for front-end functionality. 
+                    I watched a lot of tutorials to complete this project but love to reflect on how much I have accomplished with learning coding in just three short months.
                 <br></br>
                 <a className="links" href="https://acerjak.github.io/CodeQuiz/">Link to Project</a>  
                 <br></br>
@@ -60,7 +61,8 @@ const Project = () => {
             </div>
             <div className="card-reveal">
                 <span className="card-title grey-text text-darken-4 right">X</span>
-                <p>Desciption of Project here.
+                <p>This was another homework assignment for UCI coding bootcamp. The focus here was utilizing a MySQL database to ADD a burger and UPDATE a burger to a database deployed through Heroku. 
+                    I started to gain more confidence in my coding after this project, which was extremely useful during Project Two.
                 <br></br>
                 <a className="links" href="https://glacial-springs-08945.herokuapp.com/">Link to Project</a>
                 <br></br>
@@ -78,7 +80,8 @@ const Project = () => {
             </div>
              <div className="card-reveal">
                 <span className="card-title grey-text text-darken-4 right">X</span>
-                <p>Desciption of Project here.
+                <p>This was my first project during the course of UCI's coding bootcamp. I helped on both the front and back end of this project. HTML, CSS and Materialize were used for the front-end. JavaScript, Express Node Server and JQuery were used for the back-end. We utilized two API's: Mozilla WebSpeech and Parallel Dots Emotion Analysis to take the speech of the user and analyze their emotions. 
+                    The emotions were then rendered at the bottom of the application showing the text of the speech and the emotions tied to it.
                 <br></br>
                 <a className="links" href="https://maximilliancharlemagne.github.io/emotion-engine/index">Link to Project</a>   
                 <br></br>
@@ -98,7 +101,9 @@ const Project = () => {
             </div>
             <div className="card-reveal">
                 <span className="card-title grey-text text-darken-4 right">X</span>
-                <p>Desciption of Project here.
+                <p>This was one of my favorite projects during UCI's coding bootcamp. I was mostly responsbile for the back end development: Node Express Server, MySQL database with Sequelize, and Javascript. We utilized Fomantic for our styling library. 
+                    This application's focus was on how to help keep a sales person keep track of their ongoing deals in a type of tracker. This sales person would be able to add a new deal at any stage provided, but also could drag and drop to a new stage at any time.
+                    The authentication is where we hope to improve this application since the routes are not properly locked, which we recently learned can be done with Passport.
                 <br></br>
                 <a className="links" href="https://deal-tracker-app.herokuapp.com/index.html">Link to Project</a>
                 <br></br>
@@ -116,7 +121,7 @@ const Project = () => {
             </div>
              <div className="card-reveal">
                 <span className="card-title grey-text text-darken-4 right">X</span>
-                <p>Project Description here. 
+                <p>This was my first homework using ReactJS. I have only had about a week of experience working with it so far but already love it! I can't wait to learn more and build out this application even more. 
                 <br></br>
                 <a className="links" href="https://acerjak.github.io/employeedirectory/">Link to Project</a>
                 <br></br>
