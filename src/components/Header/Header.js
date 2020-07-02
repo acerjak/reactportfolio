c3984bc2e47da23884132eb2242ca4a1cea61637
acerjak <62491401+acerjak@users.noreply.github.com>
2020-07-02 09:53:48 -0700

    fixed header due to sizing issue on contact page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,14 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <nav>
-        <div className="nav-wrapper teal lighten-2">
+        <nav className="nav-wrapper teal lighten-2">
           <a href="https://acerjak.github.io/ResponsivePortfolio/index.html" className="brand-logo right hide-on-med-and-down">Amanda Cerjak</a>
           <ul className="left">
             <li><a className="waves-effect waves-light" href="/">About</a></li>
             <li><a className="waves-effect waves-light" href="/portfolio">Portfolio</a></li>
             <li><a className="waves-effect waves-light" href="/contact">Contact</a></li>
           </ul>
-        </div>
-      </nav>
+        </nav>
     )
 }
 
