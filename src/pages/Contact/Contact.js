de5aaf901910362da9e856c831541abd557ee1c8
acerjak <62491401+acerjak@users.noreply.github.com>
2020-07-13 13:43:59 -0700

    added fragment to Contactjs page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Info from '../../components/Info'
+import './Contact.css'
 
 const Contact = () => {
     return (
+        <>
         <Info />
+        </>
     )
 }
 
