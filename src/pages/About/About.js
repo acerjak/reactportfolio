4dc0a615f30e96c5db279223001977998abc724b
acerjak <62491401+acerjak@users.noreply.github.com>
2020-08-04 18:14:18 -0700

    correction Card, is actually AboutCard Component

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Card from '../../components/AboutCard'
+import AboutCard from '../../components/AboutCard'
 
 const About = () => {
     return (
         <>
-        <Card />
+        <AboutCard />
         </>
     )
 }
