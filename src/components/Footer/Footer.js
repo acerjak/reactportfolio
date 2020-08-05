701d835a47e46c52273d3c700ae3eb7408d90223
acerjak <62491401+acerjak@users.noreply.github.com>
2020-08-04 18:34:35 -0700

    added styling to stylesheet over using styling within the tagline

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer className="footer center">
-        <h4 style={{marginTop: "0px"}}>Follow Me:</h4>
+        <p className="socialTitle">Follow Me:</p>
               <a className="icons" href="https://www.linkedin.com/in/acerjak/"><img className="socialIcons" src={linkedIMG} alt="LinkedIn"/></a>
               <a className="icons" href="https://github.com/acerjak"><img className="socialIcons" src={gitIMG} alt="GitHub"/></a>
               <a className="icons igIcon" href="https://www.instagram.com/amandacerjak/?hl=en"><img className="socialIcons" src={igIMG} alt="Instagram"/></a>
